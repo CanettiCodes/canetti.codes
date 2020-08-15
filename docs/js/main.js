4a6df731dc434f49e17274973e6de9d4b36eb30c
Dan Canetti <info@danielcanetti.com>
2020-08-15 22:20:38 +0100

    Add projects.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,64 @@
-// Enable FontAwesome Pseudo
-window.FontAwesomeConfig = {
-  searchPseudoElements: true
-}
+// Slick Sliders
+$('.invisalign-go-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.align-growth-summit-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.invisalign-scientific-symposium-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.fast-track-250-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.kellars-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.emerald-kitchens-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.barnsley-squash-club-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.fit4it-fitness-images').slick({
+  arrows: false,
+  dots: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
